5c200c3f57c79d7ce18b9acda405c3850cd3b9d6
Yassine Thayer <yassine.ben.thayer.prv@gmail.com>
2024-02-05 14:20:07 +0100

    my first plot with matplotlib

--- Code-Alongs/0_first_plot.py
+++ Code-Alongs/0_first_plot.py
@@ -7,5 +7,8 @@ squares = [number**2 for number in numbers]
 print(numbers)
 print(squares)
 plt.plot(numbers , squares)
+plt.title("x^2 for for positive integers 8-9")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.show()
 
